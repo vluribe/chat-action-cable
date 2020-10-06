f07ef44dc7c6441dc89865b936a16a494719cdfc
vluribe <39499569+vluribe@users.noreply.github.com>
2020-10-06 17:40:07 -0500

    User Auth with devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root "users#index"
+  devise_for :users
   devise_for :models
+  get 'users/index'
+  resources :users
+  match '/users',   to: 'users#index',   via: 'get'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
