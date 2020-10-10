81c9d59cfca7de768b05ce88bbffcc70cfbdd53b
vluribe <39499569+vluribe@users.noreply.github.com>
2020-10-10 17:30:47 -0500

    mig

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_10_204819) do
+ActiveRecord::Schema.define(version: 2020_10_10_221406) do
 
   create_table "messages", force: :cascade do |t|
     t.text "text"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "room_id", null: false
+    t.integer "sent_by"
     t.index ["room_id"], name: "index_messages_on_room_id"
   end
 
