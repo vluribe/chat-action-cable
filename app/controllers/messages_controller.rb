d6a1c7d0c57dfeac9b3c3c93ab482c2e1903c135
vluribe <39499569+vluribe@users.noreply.github.com>
2020-10-10 16:48:21 -0500

    some changes

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,6 +1,8 @@
 class MessagesController < ApplicationController
+    before_action :authenticate_user!
     def create
         @message = Message.new(message_params)
+        @message.sent_by = current_user.id
         @message.save
         SendMessageJob.perform_later(@message)
         redirect_to request.referrer
@@ -8,6 +10,6 @@ class MessagesController < ApplicationController
 
     private
         def message_params
-            params.require(:message).permit(:text, :room_id)
+            params.require(:message).permit(:text, :room_id, :sent_by)
         end
 end
