94dd34a430bfa96f3dd372d9f933d35a37a8a8fa
vluribe <vluribe@uninorte.edu.co>
2020-10-19 15:59:30 -0500

    all notifications

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -2,6 +2,7 @@ class RoomsController < ApplicationController
     before_action :authenticate_user!
     def index
         @rooms = Room.all
+        @notifications = Notification.where(recipient_id: current_user.id).find_each 
     end
 
     def show
