ecb20197f10913f14078cfad512f6cfb4fd4bb69
Juan Conrado <djconrado@uninorte.edu.co>
2020-10-20 14:05:52 -0500

    changes made to channel

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -1,9 +1,9 @@
 class RoomChannel < ApplicationCable::Channel
   def subscribed
-    stream_from "room_channel_#{params[:room_id]}_user_#{params[:user_id]}"
+    stream_from "room_channel_#{params[:room_id]}_user_#{current_user.id}"
   end
 
   def unsubscribed
-    # Any cleanup needed when channel is unsubscribed
+    stop_all_streams
   end
 end
